b3c76a3f7da293a172ffec556641365b855e3793
jseo <ahndjds@gmail.com>
2025-05-27 17:44:46 +0900

    Initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-100 mt-8">
+            <div className="container mx-auto px-4 py-8">
+                <div className="text-center text-gray-600">
+                    <p>© 2024 쇼핑몰. All rights reserved.</p>
+                    <p className="mt-2">문의: support@shopping.com</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
