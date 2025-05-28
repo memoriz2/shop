2c04634c818f0c57f93dce69f5bdda1bf3636a39
jseo <ahndjds@gmail.com>
2025-05-28 18:10:45 +0900

    Move frontend files to new directory

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NszSvxmXIhHVgiFdyQEWl8kfKSAa4nr1IoGQtRqIzhY=\"\n}"
--- .next/static/chunks/app_page_tsx_98a7fc26._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a683ab65._.js",
-  "static/chunks/components_css_4dabc682._.css"
-],
-    source: "dynamic"
-});
