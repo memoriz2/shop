03b1884e9d58eb97b2310a404f027ad67660b532
jseo <ahndjds@gmail.com>
2025-06-06 12:32:03 +0900

    상세보기

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,31 @@
 "use client";
 
 import "./css/Main.css";
+import { useState } from "react";
+
+export default function Main({}) {
+    type Product = {
+        productId: number;
+        productName: string;
+        productPhoto?: string;
+    };
+
+    const [selectedProduct, setSelectedProduct] = useState<Product | null>(
+        null
+    );
+
+    if (selectedProduct) {
+        return (
+            <main>
+                <h2>{selectedProduct.productName}</h2>
+                <p>상품 ID: {selectedProduct.productId}</p>
+                <img
+                    src={selectedProduct.productPhoto}
+                    alt={selectedProduct.productName}
+                />
+            </main>
+        );
+    }
+
+    return <main></main>;
+}
