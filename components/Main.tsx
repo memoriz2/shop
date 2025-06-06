2c4b7c5386f3eba5eae220cd7849de06b0fa8f19
jseo <ahndjds@gmail.com>
2025-06-06 12:39:24 +0900

    painting 상세보기 추가

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,6 +2,7 @@
 
 import "./css/Main.css";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Main({}) {
     type Product = {
@@ -10,22 +11,18 @@ export default function Main({}) {
         productPhoto?: string;
     };
 
-    const [selectedProduct, setSelectedProduct] = useState<Product | null>(
-        null
-    );
+    const [selectedProduct] = useState<Product | null>(null);
 
     if (selectedProduct) {
         return (
             <main>
                 <h2>{selectedProduct.productName}</h2>
                 <p>상품 ID: {selectedProduct.productId}</p>
-                <img
-                    src={selectedProduct.productPhoto}
+                <Image
+                    src={selectedProduct.productPhoto || "/default.jpg"}
                     alt={selectedProduct.productName}
                 />
             </main>
         );
     }
-
-    return <main></main>;
 }
