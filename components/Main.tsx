a667ffc30b6c68a2349fb0a322cd81939e31a8d5
jseo <ahndjds@gmail.com>
2025-06-06 14:19:03 +0900

    상세보기 슬라이더/페이지 가운데 정렬 및 화살표 스타일 개선

--- components/Main.tsx
+++ components/Main.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styles from "./css/Main.module.css";
 
 export type ProductPhoto = {
     url: string;
@@ -46,52 +47,59 @@ export default function Main() {
             <Header onProductSelect={setSelectedProduct} products={products} />
             <main>
                 {selectedProduct && (
-                    <div>
+                    <div className={styles.detailWrapper}>
                         <h2>{selectedProduct.productName}</h2>
                         <p>상품 ID: {selectedProduct.productId}</p>
-                        {Array.isArray(selectedProduct.productPhoto) &&
-                        selectedProduct.productPhoto.length > 0 ? (
-                            <Slider {...sliderSettings}>
-                                {selectedProduct.productPhoto.map(
-                                    (photo: ProductPhoto, idx: number) => (
-                                        <div key={idx}>
-                                            <Image
-                                                src={photo.url}
-                                                alt={
-                                                    photo.filename ||
-                                                    selectedProduct.productName
-                                                }
-                                                width={400}
-                                                height={300}
-                                            />
-                                        </div>
-                                    )
-                                )}
-                            </Slider>
-                        ) : selectedProduct.productPhoto &&
-                          (selectedProduct.productPhoto as ProductPhoto).url ? (
-                            <Image
-                                src={
-                                    (
-                                        selectedProduct.productPhoto as ProductPhoto
-                                    ).url
-                                }
-                                alt={
-                                    (
-                                        selectedProduct.productPhoto as ProductPhoto
-                                    ).filename || selectedProduct.productName
-                                }
-                                width={400}
-                                height={300}
-                            />
-                        ) : (
-                            <Image
-                                src={"/default.jpg"}
-                                alt={selectedProduct.productName}
-                                width={400}
-                                height={300}
-                            />
-                        )}
+                        <div className={styles.sliderWrapper}>
+                            {Array.isArray(selectedProduct.productPhoto) &&
+                            selectedProduct.productPhoto.length > 0 ? (
+                                <Slider {...sliderSettings}>
+                                    {selectedProduct.productPhoto.map(
+                                        (photo: ProductPhoto, idx: number) => (
+                                            <div
+                                                key={idx}
+                                                className={styles.slideItem}
+                                            >
+                                                <Image
+                                                    src={photo.url}
+                                                    alt={
+                                                        photo.filename ||
+                                                        selectedProduct.productName
+                                                    }
+                                                    width={400}
+                                                    height={300}
+                                                />
+                                            </div>
+                                        )
+                                    )}
+                                </Slider>
+                            ) : selectedProduct.productPhoto &&
+                              (selectedProduct.productPhoto as ProductPhoto)
+                                  .url ? (
+                                <Image
+                                    src={
+                                        (
+                                            selectedProduct.productPhoto as ProductPhoto
+                                        ).url
+                                    }
+                                    alt={
+                                        (
+                                            selectedProduct.productPhoto as ProductPhoto
+                                        ).filename ||
+                                        selectedProduct.productName
+                                    }
+                                    width={400}
+                                    height={300}
+                                />
+                            ) : (
+                                <Image
+                                    src={"/default.jpg"}
+                                    alt={selectedProduct.productName}
+                                    width={400}
+                                    height={300}
+                                />
+                            )}
+                        </div>
                         {selectedProduct.description && (
                             <p style={{ marginTop: 20 }}>
                                 {selectedProduct.description}
